3767e03edaa3cce44e49899a4f1d4fd21a0be29b
M-Mustufa-Khan <mustufakhan0318@gmail.com>
2025-09-25 14:13:35 +0500

    calculater error changes

--- calc.js
+++ calc.js
@@ -1,20 +1,26 @@
 const display = document.querySelector(".display")
 
-function number (num){
+function number(num) {
     display.value += num
 }
 
-function getResult(){
-    try {
-        display.value = eval(display.value.replace(/x/g, "*"));
-    } catch {
-        display.value = "Error";
+function getResult() {
+    if (display.value.length === 0) {
+        display.value = "";
+    } else {
+        try {
+            display.value = eval(display.value.replace(/x/g, "*"));
+
+        } catch {
+            display.value = "";
+        }
+
     }
 }
-function clearResult(){
+function clearResult() {
     display.value = "";
 }
 
-function removeChar(){
+function removeChar() {
     display.value = display.value.slice(0, -1);
 }
