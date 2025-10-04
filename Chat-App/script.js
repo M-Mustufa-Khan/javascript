2f5c578dd500b6207625c28c0a5aa8dfb060a819
M-Mustufa-Khan <mustufakhan0318@gmail.com>
2025-10-04 19:45:47 +0500

    chat app data adding bot or user

--- Chat-App/script.js
+++ Chat-App/script.js
@@ -1,1 +1,25 @@
+const bot = document.getElementsByClassName("chat-box")[0]
+const userInput = document.getElementsByClassName("userInput")
 
+const messageList = [
+    {
+        id: "bot",
+        message: "Hi"
+    },
+    {
+        id: "user",
+        message: "Hello"
+    },
+    {
+        id: "bot",
+        message: "What's the Agenda Today"
+    },
+    {
+        id: "user",
+        message: "Ja Bhai Kaam Kr"
+    },
+    {
+        id: "bot",
+        message: "Chal Bhai ja yr"
+    },
+]
