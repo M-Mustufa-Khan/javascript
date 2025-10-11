e23aeb29770702c9fc74d481eba493a06ff67c68
M-Mustufa-Khan <mustufakhan0318@gmail.com>
2025-10-11 15:04:56 +0500

    make three pages about blog or contact and chages the picture of all products

--- Ecommerce-Website/script.js
+++ Ecommerce-Website/script.js
@@ -1,15 +1,41 @@
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const close = document.getElementById("close");
+const smallImg = document.querySelectorAll(".small-img");
+const mainImg = document.getElementById("mainImg");
+const products = document.querySelectorAll(".pro");
+const sImg = document.querySelectorAll(".small-img-col img");
 
+// Side Navbar
 if (bar) {
   bar.addEventListener("click", () => {
     nav.classList.add("active");
   });
 }
 
+// Close Side Bar
 if (close) {
   close.addEventListener("click", () => {
     nav.classList.remove("active");
   });
 }
+
+// Small Image to Big Image
+// Active Effect jb picture jo open hogi uski duplicate matlb choti picture higlight hogi
+
+if (mainImg && smallImg.length > 0) {
+  smallImg.forEach((img) => {
+    img.addEventListener("click", () => {
+      mainImg.src = img.src;
+      smallImg.forEach((thumb) => thumb.classList.remove("active"));
+      img.classList.add("active");
+    });
+  });
+}
+
+// Product to product details Page
+products.forEach((product) => {
+  product.addEventListener("click", () => {
+    window.location.href = "sproduct.html";
+  });
+});
