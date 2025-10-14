46f12cc3bc8e90de2d6644eb00df58de2e20f4ee
M-Mustufa-Khan <mustufakhan0318@gmail.com>
2025-10-14 16:50:41 +0500

    different folders

--- task/script.js
+++
@@ -1,3 +0,0 @@
-let names = "muhammad fareed";
-let fname = names.split("");
-console.log("Last Letter:", names[names.length - 1], fname[fname.length - 1]);
---
+++ task/script6.js
@@ -0,0 +1,3 @@
+let names = "muhammad fareed";
+let fname = names.split("");
+console.log("Last Letter:", names[names.length - 1], fname[fname.length - 1]);
