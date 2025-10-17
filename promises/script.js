8a641fb3c89a7a946440a0e25df1f7b4b263e97b
M-Mustufa-Khan <mustufakhan0318@gmail.com>
2025-10-17 16:58:03 +0500

    add setTimeout

--- promises/script.js
+++ promises/script.js
@@ -3,19 +3,26 @@
 let complete = true
 
 let prom = new Promise(function(resolve, reject){
-    if (complete) {
+    console.log("Fetching Data, Please Wait...");
+    setTimeout(() => {
+        if (complete) {
         resolve("Here is an Resolvetion")
     }else{
         reject("Here is an Rejection")
     }
+    }, 3000)
 })
 
-let onFullfilment = (result) => {
-    console.log(result);
-}
-let onRejection = (error) => {
-    console.log(error);
-}
+// let onFullfilment = (result) => {
+//     console.log(result);
+// }
+// let onRejection = (error) => {
+//     console.log(error);
+// }
 
-prom.then(onFullfilment)
-prom.catch(onRejection)
+prom.then((result) => {
+    console.log(result);
+})
+prom.catch((error) =>{
+    console.log(error);
+})
