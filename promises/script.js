2a3d40be4511884445354d298c0c41b346d436dd
M-Mustufa-Khan <mustufakhan0318@gmail.com>
2025-10-17 17:32:19 +0500

    learn async await

--- promises/script.js
+++ promises/script.js
@@ -1,28 +1,35 @@
 // Promises practice
 
-let complete = true
+let complete = true;
 
-let prom = new Promise(function(resolve, reject){
-    console.log("Fetching Data, Please Wait...");
-    setTimeout(() => {
-        if (complete) {
-        resolve("Here is an Resolvetion")
-    }else{
-        reject("Here is an Rejection")
+let prom = new Promise(function (resolve, reject) {
+  console.log("Fetching Data, Please Wait...");
+  setTimeout(() => {
+    if (complete) {
+      resolve("Here is an Resolvetion");
+    } else {
+      reject("Here is an Rejection");
     }
-    }, 3000)
-})
+  }, 3000);
+});
 
 // let onFullfilment = (result) => {
 //     console.log(result);
-// }
+// };
 // let onRejection = (error) => {
 //     console.log(error);
-// }
+// };
 
-prom.then((result) => {
-    console.log(result);
-})
-prom.catch((error) =>{
-    console.log(error);
-})
+// prom.then((result) => {
+//     console.log(result);
+// });
+// prom.catch((error) =>{
+//     console.log(error);
+// });
+
+async function getData() {
+  let data = await prom;
+  console.log(data);
+}
+
+getData();
